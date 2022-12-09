d6081d876e9caa87e4ad140f8dec5abc2e37f62f
Masocher <Masochergamer@gmail.com>
2022-12-09 15:14:00 +0330

    contact-us page / title box created

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -5,6 +5,45 @@ const ContactUs = () => {
     return (
         <div className="contact_us_container">
             <Header />
+
+            <div className="contact_us_title_box">
+                <div className="contact_us_title">ارتباط با ما</div>
+                <div className="contact_us_title_des">در این صفحه میتوانید اطلاعات ارتباطی چوکو را مشاهده کنید</div>
+                <hr className="box_bottom_border" />
+            </div>
+
+            
+
+            <style jsx>{`
+                .contact_us_title_box {
+                    text-align: center;
+                    margin: 40px 0 20px 0;
+                }
+
+                .contact_us_title {
+                    font-size: 50px;
+                    color: #757575;
+                    margin: 0;
+                    padding: 0;
+                }
+
+                .contact_us_title_des {
+                    font-size: 18px;
+                    font-weight: 100;
+                    color: #505050;
+                    position: relative;
+                    bottom: 5px;
+                }
+
+                .box_bottom_border {
+                    border: none;
+                    height: 1px;
+                    background-color: #ff9000;
+                    width: 4%;
+                    margin: 10px auto 0 auto;
+                    opacity: 1;
+                }
+            `}</style>
         </div>
     )
 }

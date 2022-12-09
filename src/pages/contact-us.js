6bb86cbbf7eade97d87b3c91f57dc1a67ce85326
Masocher <Masochergamer@gmail.com>
2022-12-09 14:17:00 +0330

    contact-us page created

---
+++ src/pages/contact-us.js
@@ -0,0 +1,12 @@
+// components
+import Header from "./../components/header"
+
+const ContactUs = () => {
+    return (
+        <div className="contact_us_container">
+            <Header />
+        </div>
+    )
+}
+
+export default ContactUs

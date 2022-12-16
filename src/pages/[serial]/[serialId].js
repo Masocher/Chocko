e8e86e79734459457a2cf6777312d50e6e8320bf
Masocher <Masochergamer@gmail.com>
2022-12-16 19:46:25 +0330

    serial box / meta box created

--- src/pages/[serial]/[serialId].js
+++ src/pages/[serial]/[serialId].js
@@ -8,6 +8,10 @@ import { useSelector } from "react-redux"
 import Header from "../../components/header"
 import Footer from "../../components/footer"
 
+// fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBookmark, faH, faHeart } from "@fortawesome/free-solid-svg-icons"
+
 const SerialPage = () => {
     const router = useRouter()
 
@@ -20,8 +24,18 @@ const SerialPage = () => {
             <Header />
 
             <div className="serial_box">
-                <div className="serial_cover"></div>
-                <div className="serial_title">{serialInformation.category}-{router.query.serialId}</div>
+                <div className="right_side">
+                    <div className="serial_cover"></div>
+                    
+                    <div className="right_side_information">
+                        <div className="meta_box">
+                            <div className="icon_box"><FontAwesomeIcon icon={faBookmark} /></div>
+                            <div className="icon_box"><FontAwesomeIcon icon={faHeart} /></div>
+                            <div className="manufacturing_country">{serialInformation.manufacturingCountry}</div>
+                        </div>
+                        <div className="serial_title">سریال - {router.query.serialId}</div>
+                    </div>
+                </div>
             </div>
 
             <Footer />
@@ -31,7 +45,15 @@ const SerialPage = () => {
                     width: calc(100% - 100px);
                     direction: rtl;
                     margin: 50px;
+                }
+
+                .right_side {
                     display: flex;
+                    height: 100%;
+                }
+
+                .right_side_information {
+                    height: 100%;
                 }
 
                 .serial_cover {
@@ -39,6 +61,53 @@ const SerialPage = () => {
                     border-radius: 10px;
                     width: 275px;
                     height: 350px;
+                    margin-left: 40px;
+                }
+
+                .meta_box {
+                    display: flex;
+                    align-items: center;
+                    height: fit-content;
+                    width: 200px;
+                    justify-content: space-between;
+                    color: #757575;
+                }
+
+                .icon_box {
+                    width: 35px;
+                    height: 35px;
+                    font-size: 20px;
+                    border: solid #757575;
+                    border-width: 0 0 1px 0;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    padding-bottom: 3px;
+                }
+
+                .manufacturing_country {
+                    font-size: 14px;
+                    border: solid #757575;
+                    border-width: 0 0 1px 0;
+                    padding: 0 10px;
+                    cursor: pointer;
+                    transition: .2s;
+                    height: 35px;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    padding-bottom: 3px;
+                }
+                .manufacturing_country:hover {
+                    color: #ff9000;
+                    border-color: #ff9000;
+                }
+
+                .serial_title {
+                    font-size: 30px;
+                    color: #fff;
+                    font-weight: 100;
+                    margin-top: 30px;
                 }
             `}</style>
         </div>

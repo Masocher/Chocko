e1d7f6393a86d726e788b85d68c74281d469a6f3
Masocher <Masochergamer@gmail.com>
2022-12-17 14:54:51 +0330

    stars styled

--- src/components/stars.js
+++ src/components/stars.js
@@ -1,15 +1,61 @@
+// swiper
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 const Stars = ({ stars }) => {
     return (
-        <div className="stars_container">
+        <Swiper
+            className="stars_container"
+            style={{
+                width: "calc(100% - 100px)",
+                height: "150px",
+                paddingBottom: "40px",
+                margin: "0 50px 0 50px"
+            }}
+            slidesPerView={8}
+            spaceBetween={30}
+            modules={[Navigation]}
+            navigation
+        >
             {
                 stars.map(star =>
-                    <div className="star_box">
+                    <SwiperSlide
+                        className="star_box"
+                        style={{
+                            width: "150px",
+                            height: "100%",
+                            position: "relative"
+                        }}
+                    >
                         <div className="star_img"></div>
-                        <div className="star_name">{ star.name }</div>
-                    </div>
+                        <div className="star_name">{star.name}</div>
+                    </SwiperSlide>
                 )
             }
-        </div>
+
+            <style jsx>{`
+                .star_img {
+                    background-color: #111;
+                    width: 100%;
+                    height: 100%;
+                    border-radius: 50%;
+                }
+
+                .star_name {
+                    font-size: 14px;
+                    letter-spacing: .5px;
+                    font-weight: 100;
+                    position: absolute;
+                    left: 50%;
+                    transform: translate(-50%, 0);
+                    bottom: 10px;
+                    color: #fff;
+                    bottom: -38px;
+                }
+            `}</style>
+        </Swiper>
     )
 }
 

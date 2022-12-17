ac98c30eb0f0f941af25b3916327f9dca7c41942
Masocher <Masochergamer@gmail.com>
2022-12-17 15:34:08 +0330

    update stars slider

--- src/components/stars.js
+++ src/components/stars.js
@@ -24,46 +24,49 @@ const Stars = ({ stars }) => {
 
     const windowWidthSize = (width) => {
         if (width >= 1536) {
-            return { perview: 7, spaceBetween: 60, height: "150px" }
+            return { perview: 7, spaceBetween: 45, height: "150px" }
         } else if (width < 1536 && width >= 1200) {
             return { perview: 6, spaceBetween: 50, height: "180px" }
         } else if (width < 1200 && width >= 1100) {
             return { perview: 5, spaceBetween: 40, height: "150px" }
         } else if (width < 1100 && width >= 970) {
-            return { perview: 5, spaceBetween: 40, height: "150px" }
+            return { perview: 5, spaceBetween: 45, height: "130px" }
         } else if (width < 970 && width >= 820) {
-            return { perview: 4, spaceBetween: 20, height: "150px" }
+            return { perview: 4, spaceBetween: 20, height: "130px" }
         } else if (width < 820 && width >= 700) {
-            return { perview: 4, spaceBetween: 25, height: "150px" }
+            return { perview: 4, spaceBetween: 25, height: "130px" }
         } else if (width < 700 && width >= 590) {
-            return { perview: 3, spaceBetween: 20, height: "150px" }
+            return { perview: 3, spaceBetween: 20, height: "130px" }
         } else if (width < 590 && width >= 430) {
-            return { perview: 2, spaceBetween: 110, height: "150px" }
+            return { perview: 2, spaceBetween: 110, height: "130px" }
         } else if (width < 430 && width >= 380) {
-            return { perview: 2, spaceBetween: 20, height: "150px" }
+            return { perview: 2, spaceBetween: 20, height: "110px" }
         } else if (width < 380 && width >= 330) {
-            return { perview: 2, spaceBetween: 30, height: "120px" }
+            return { perview: 1, spaceBetween: 10, height: "180px" }
         } else if (width < 330 && width >= 0) {
-            return { perview: 2, spaceBetween: 30, height: "95px" }
+            return { perview: 2, spaceBetween: 10, height: "125px" }
         } else {
             return { perview: 10, spaceBetween: 10 }
         }
     }
 
+    console.log(windowWidth);
+
     return (
         <Swiper 
             className="stars_container"
             style={{
-                width: "calc(100% - 100px)",
+                width: `${windowWidth <= 321 ? 'calc(100% - 50px)' : 'calc(100% - 200px)'}`,
                 height: windowWidthSize(windowWidth).height,
-                paddingBottom: "40px",
-                margin: "0 50px 0 50px"
+                padding: `${windowWidth <= 321 ? '40px 0' : '40px 60px'}`,
+                margin: "0 auto 0 auto",
             }}
             slidesPerView={windowWidthSize(windowWidth).perview}
             spaceBetween={windowWidthSize(windowWidth).spaceBetween}
             modules={[Navigation]}
             navigation
         >
+            <div className="stars_slider_title">بازیگران</div>
             {
                 stars.map(star =>
                     <SwiperSlide
@@ -89,6 +92,13 @@ const Stars = ({ stars }) => {
                     padding-bottom: 30px;
                 }
 
+                .stars_slider_title {
+                    font-size: 20px;
+                    position: absolute;
+                    top: -20px;
+                    right: 0;
+                }
+
                 .star_box {
                     width: 100px;
                     height: 100px;
@@ -99,7 +109,7 @@ const Stars = ({ stars }) => {
                     background-color: #111;
                     width: 100%;
                     height: 100%;
-                    border-radius: 50%;
+                    border-radius: 20px;
                 }
 
                 .star_name {
@@ -112,6 +122,13 @@ const Stars = ({ stars }) => {
                     bottom: 10px;
                     color: #fff;
                     bottom: -38px;
+                }
+
+                @media (max-width: 1025px) {
+                    .star_name {
+                        width: 100%;
+                        text-align: center;
+                    }
                 }
 
                 @media (max-width: 770px) {

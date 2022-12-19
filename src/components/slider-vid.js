82f8d2912714c4f6afeed63dd70f724bae533746
Masocher <Masochergamer@gmail.com>
2022-12-19 14:07:07 +0330

    update items dynamic routes linking

--- src/components/slider-vid.js
+++ src/components/slider-vid.js
@@ -70,7 +70,7 @@ const SliderVid = ({ title, information }) => {
                 {
                     information.map(inf =>
                         <SwiperSlide key={inf.id} style={{ width: "180px", height: "100%" }}>
-                            <Link href={`/${inf.categoryNumber}/${inf.id}`}>
+                            <Link href="/[itemCategory]/[itemId]" as={`/${inf.categoryNumber}/${inf.id}`}>
                                 <div className="slide_content">
                                     <div className="slide_title">{inf.category}</div>
                                 </div>

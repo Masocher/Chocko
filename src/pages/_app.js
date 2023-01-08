447a9471780013ec6f465eb813ac9649e438dfc1
Masocher <Masochergamer@gmail.com>
2023-01-08 17:25:46 +0330

    use the new font in project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,9 @@ import Layout from "./../components/layout"
 import store from "../redux/store"
 import { Provider } from "react-redux"
 
+// styles
+import './../../public/styles/main.css'
+
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>

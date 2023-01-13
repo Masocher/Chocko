ca9b686e0ea6e52c634fd46811d96db6ea3fdbd8
Masocher <Masochergamer@gmail.com>
2023-01-13 12:22:08 +0330

    create pannel / profile section / user's about box

--- src/components/pannel sections/profile-box.js
+++ src/components/pannel sections/profile-box.js
@@ -8,6 +8,10 @@ import cover_img from './../../../public/images/user-images/cover-img.jpg'
 // tools
 import Image from 'next/legacy/image'
 
+// fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCalendarAlt, faRotate } from "@fortawesome/free-solid-svg-icons"
+
 const ProfileBox = () => {
 
     const dispatch = useDispatch()
@@ -28,20 +32,28 @@ const ProfileBox = () => {
 
                             <div className="user_information_box_2">
                                 <div className="user_last_activity">
+                                    <div className="clock_icon"><FontAwesomeIcon icon={faRotate} /></div>
                                     <div className="last_activity">آخرین فعالیت</div>
                                     <div className="last_activity_date">۱۴۰۱<span>/</span>۱۰<span>/</span>۲۰</div>
-                                    <div className="clock_icon"></div>
                                 </div>
 
                                 <div className="user_registery_date">
+                                <div className="calendar_icon"><FontAwesomeIcon icon={faCalendarAlt} /></div>
                                     عضو از
                                     <div className="registery_date">۲ سال پیش</div>
-                                    <div className="calendar_icon"></div>
                                 </div>
                             </div>
                         </div>
 
                         <div className="edit_profile_information" onClick={() => dispatch(openDashboardSections(7))}>ویرایش پروفایل</div>
+                    </div>
+                </div>
+
+                <div className="user_about_box">
+                    <div className="user_about_title"><span>.</span> درباره من</div>
+
+                    <div className="user_about_description">
+                        طراح گرافیک از این نوشتار به‌عنوان عنصری از ترکیب‌ بندی برای پر کردن صفحه و ارائه اولیه شکل ظاهری استفاده میکند.
                     </div>
                 </div>
             </div>
@@ -51,7 +63,11 @@ const ProfileBox = () => {
                     width: calc(100% - 420px);
                     height: 100%;
                     display: none;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: flex-start;
                     position: relative;
+                    padding: 0 50px;
                 }
                 .profile_container.show {
                     display: flex;
@@ -84,7 +100,7 @@ const ProfileBox = () => {
                 .edit_profile_information {
                     background-color: #ff9000;
                     color: #000;
-                    padding: 8px 15px;
+                    padding: 8px 25px;
                     border-radius: 5px;
                     border: 1px solid #ff9000;
                     font-size: 14px;
@@ -165,6 +181,39 @@ const ProfileBox = () => {
                 }
                 .registery_date {
                     margin-right: 3px
+                }
+
+                .calendar_icon, .clock_icon {
+                    color: #ff9000;
+                    margin: 1px 0 0 7px;
+                }
+
+                .user_about_box {
+                    width: calc(100% - 100px);
+                    padding: 20px 50px 40px 50px;
+                    background-color: #111;
+                    border-radius: 10px;
+                    margin-top: 15px;
+                }
+
+                .user_about_title {
+                    font-size: 30px;
+                    letter-spacing: .5px;
+                    padding-right: 20px;
+                    position: relative;
+                }
+
+                .user_about_title span {
+                    color: #ff9000;
+                    font-size: 50px;
+                    position: absolute;
+                    top: -22px;
+                    right: -1px;
+                }
+
+                .user_about_description {
+                    font-family: Vazir;
+                    margin-top: 15px;
                 }
             `}</style>
         </div>

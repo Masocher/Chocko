1b604c6c0e48911e2168cfd41a5905702bdb7c60
Masocher <Masochergamer@gmail.com>
2023-01-17 03:09:32 +0330

    the pannel / movie-request section is responsive

--- src/components/pannel sections/movie-request.js
+++ src/components/pannel sections/movie-request.js
@@ -62,13 +62,14 @@ const MovieRequest = () => {
                     color: #fff;
                     outline: 0;
                     border-radius: 5px;
+                    border: 1px solid #000;
                 }
                 .movie_req_input::placeholder {
                     color: #999;
                     font-size: 13px;
                 }
                 .movie_req_input:focus {
-                    border-color: #444;
+                    border-color: #ff9000;
                 }
 
                 .movie_name {
@@ -86,6 +87,56 @@ const MovieRequest = () => {
 
                 .submit_btn {
                     padding: 5px 20px;
+                }
+
+                @media (max-width: 1025px) {
+                    .movie_request_container {
+                        width: calc(100% - 320px);
+                    }
+                }
+
+                @media (max-width: 880px) {
+                    .movie_request_container {
+                        width: 100%;
+                    }
+
+                    .movie_request_box {
+                        width: calc(100% - 80px);
+                        height: calc(100% - 80px);
+                        justify-content: center;
+                    }
+                }
+
+                @media (max-width: 769px) {
+                    .movie_request_container {
+                        width: 100%;
+                        height: 640px;
+                    }
+
+                    .movie_request_box {
+                        margin-top: 115px;
+                    }
+                }
+
+                @media (max-width: 426px) {
+                    .movie_request_box {
+                        margin-top: 40px;
+                    }
+
+                    .about_movie {
+                        height: 100px;
+                    }
+                }
+
+                @media (max-width: 321px) {
+                    .title {
+                        font-size: 24px;
+                    }
+
+                    .movie_request_form {
+                        width: calc(100% - 40px);
+                        padding: 0 20px;
+                    }
                 }
             `}</style>
         </div>
